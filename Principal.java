471b5d0a7379daf0695738a2fefe58912bf008ac
LuanAngeloRocha <Luanrochaangelo@gmail.com>
2022-03-08 19:18:10 +0000

    adicionando

---
+++ Principal.java
@@ -0,0 +1,8 @@
+public class Principal {
+    public static void main(String[] args) {
+        Carro c = new Carro();
+        
+        System.out.println(c.getFabricante());
+        System.out.println(c.getNome());
+    }
+}
